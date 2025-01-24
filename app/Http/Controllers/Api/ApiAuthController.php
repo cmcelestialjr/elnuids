44e39318e80e183efd5f6e756ae378eb0c86b307
cesar <cesar.celestialjr@lnu.edu.ph>
2025-01-24 18:56:20 +0800

    update

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -14,73 +14,74 @@ class ApiAuthController extends Controller
 {
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'username' => 'required|string',
-            'password' => 'required|string',
-        ]);
+        // $validator = Validator::make($request->all(), [
+        //     'username' => 'required|string',
+        //     'password' => 'required|string',
+        // ]);
 
-        if ($validator->fails()) {
-            return response()->json(['error' => 'Invalid data'], 400);
-        }        
+        // if ($validator->fails()) {
+        //     return response()->json(['error' => 'Invalid data'], 400);
+        // }        
         
-        $apiToken = $request->header('App-Token');
-        $apiKey = config('app.api_key');
+        // $apiToken = $request->header('App-Token');
+        // $apiKey = config('app.api_key');
 
-        if ($apiToken !== $apiKey) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
+        // if ($apiToken !== $apiKey) {
+        //     return response()->json(['error' => 'Unauthorized'], 401);
+        // }
 
-        $username = $request->username;
-        $password = $request->password;
-        #$password = $this->decryptPassword($request->password);
+        // $username = $request->username;
+        // $password = $request->password;
+        // #$password = $this->decryptPassword($request->password);
 
-        $credentials = ['username' => $username, 'password' => $password];
-        #return response()->json(['error' => $password], 401);
-        if (Auth::attempt($credentials)) {
-            $user = User::where('username',$username)->first();
-            $user->tokens()->delete();
-            $token = $user->createToken(
-                'LNU-IDS-APP', ['*'], Carbon::now()->addDay(),
-            )->plainTextToken;
+        // $credentials = ['username' => $username, 'password' => $password];
+        // #return response()->json(['error' => $password], 401);
+        // if (Auth::attempt($credentials)) {
+        //     $user = User::where('username',$username)->first();
+        //     $user->tokens()->delete();
+        //     $token = $user->createToken(
+        //         'LNU-IDS-APP', ['*'], Carbon::now()->addDay(),
+        //     )->plainTextToken;
             
-            $user1 = User::find($user->id);
-            $user1->api_token = $token;
-            $user1->save();
+        //     $user1 = User::find($user->id);
+        //     $user1->api_token = $token;
+        //     $user1->save();
 
-            $role = 0;
+        //     $role = 0;
 
-            $user_systems = UsersNavigation::select('system')
-                ->where('user_id',$user->id)
-                ->where('nav',"")
-                ->where('nav_sub',"")
-                ->whereIn('system',['SIMS','HRIMS','FIS','DTS'])
-                ->groupBy('system')
-                ->get();
-            $systems = [];
-            if($user_systems->count()>0){
-                foreach($user_systems as $row){
-                    if (!in_array($row->system, $systems)) {
-                        $systems[] = $row->system;
-                    }
-                }
-            }
+        //     $user_systems = UsersNavigation::select('system')
+        //         ->where('user_id',$user->id)
+        //         ->where('nav',"")
+        //         ->where('nav_sub',"")
+        //         ->whereIn('system',['SIMS','HRIMS','FIS','DTS'])
+        //         ->groupBy('system')
+        //         ->get();
+        //     $systems = [];
+        //     if($user_systems->count()>0){
+        //         foreach($user_systems as $row){
+        //             if (!in_array($row->system, $systems)) {
+        //                 $systems[] = $row->system;
+        //             }
+        //         }
+        //     }
 
             return response()->json([
-                'username' => $user->username,
-                'role' => $role,
-                'id_no' => $user->id_no,
-                'stud_id' => $user->stud_id,
-                'lastname' => $user->lastname,
-                'firstname' => $user->firstname,
-                'middlename' => $user->middlename,
-                'extname' => $user->extname,
-                'token' => $token,
-                'systems' => $systems
+                'username' => 'gsa'
+                // 'username' => $user->username,
+                // 'role' => $role,
+                // 'id_no' => $user->id_no,
+                // 'stud_id' => $user->stud_id,
+                // 'lastname' => $user->lastname,
+                // 'firstname' => $user->firstname,
+                // 'middlename' => $user->middlename,
+                // 'extname' => $user->extname,
+                // 'token' => $token,
+                // 'systems' => $systems
             ], 200);
 
-        } else {
-            return response()->json(['error' => 'Invalid Username and Password'], 401);
-        }
+        // } else {
+        //     return response()->json(['error' => 'Invalid Username and Password'], 401);
+        // }
     }
 
     public function decryptPassword($encryptedData) {
