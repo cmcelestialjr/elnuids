b0fef0961c059ea23f65db73ed615b2cf73e070d
cesar <cesar.celestialjr@lnu.edu.ph>
2025-01-27 15:15:04 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,1 @@
-{{-- <iframe id="documentPreview" src="{{ route('payslip') }}#zoom=120" style="width:100%; height: 600px;" frameborder="0"></iframe> --}}
-{{public_path('storage/Payslip/2025/101250100001.pdf')}}
+<iframe id="documentPreview" src="{{ public_path('storage/Payslip/2025/101250100001.pdf') }}#zoom=120" style="width:100%; height: 600px;" frameborder="0"></iframe>
