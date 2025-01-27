c88b6c66902b44380b6b6a064d26471e3e90fe22
cesar <cesar.celestialjr@lnu.edu.ph>
2025-01-27 09:18:03 +0800

    update

--- app/Models/UsersNavigation.php
+++ app/Models/UsersNavigation.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsersNavigation extends Model
 {
-    protected $table = 'users_navigations';
+    protected $table = 'users_navigation';
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id')->withDefault();
